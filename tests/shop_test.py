822f99046423c3af74a6883fb3ad3826a1202356
javiser <javiser@users.noreply.github.com>
2021-11-27 21:35:13 +0100

    Added some very basic tests for the Shop class

--- tests/shop_test.py
+++ tests/shop_test.py
@@ -1,1 +1,15 @@
 from scraper.shops import Shop
+
+
+def test_shops_list_not_empty():
+    assert len(Shop.get_shops_list()) > 0
+
+
+def test_shops_scrape_from_non_existing_shop():
+    product = Shop.get_product_from_url("https://www.some-dummy-address.com")
+    assert not product
+
+
+def test_shops_scrape_from_invalid_address():
+    product = Shop.get_product_from_url("https://www.notebooksbilliger.de")
+    assert not product.name
